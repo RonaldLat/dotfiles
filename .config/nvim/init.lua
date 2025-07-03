3eb2fb7ccb0ccb1aa333e0e28277f14cda91b1c5
Otieno <otieno@codemini.local>
2025-07-03 10:40:05 +0300

    fix bug in blink-cmp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,25 @@ require("autocmds")
 -- require("mason").setup()
 -- require("lspsaga.symbol.winbar").get_bar()
 
+local odools = require("odools")
+local h = os.getenv("HOME")
+odools.setup({
+	-- mandatory
+	odoo_path = h .. "/src/odoo/",
+	python_path = h .. "/.pyenv/shims/python3",
+
+	-- optional
+	server_path = h .. "/.local/share/nvim/odoo/odoo_ls_server",
+	addons = { h .. "/src/enterprise/" },
+	additional_stubs = { h .. "/src/additional_stubs/", h .. "/src/typeshed/stubs" },
+	root_dir = h .. "/src/", -- working directory, odoo_path if empty
+	settings = {
+		autoRefresh = true,
+		autoRefreshDelay = nil,
+		diagMissingImportLevel = "none",
+	},
+})
+
 -- vim.cmd("colorscheme pywal16")
 vim.cmd("colorscheme everforest")
 -- vim.cmd("colorscheme monokai-pro")
---
+++ .config/nvim/lua/plugins/colorschemes/catpuccin.lua
@@ -0,0 +1,1 @@
+return { "catppuccin/nvim", event = { "VeryLazy" }, name = "catppuccin", priority = 1000 }
