31caded309ce5ee510dec7e337a82b887fc7732f
Ronald Otieno <ronnlats@gmail.com>
2025-07-14 06:49:20 +0300

    working blink-cmp

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,81 +1,178 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		"Kaiser-Yang/blink-cmp-dictionary",
-	},
-	event = { "VeryLazy" },
-
-	-- version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+	"Saghen/blink.cmp",
+	event = "InsertEnter",
 	build = "cargo build --release",
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		-- keymap = { preset = 'default' },
-		keymap = {
-			preset = "super-tab",
-
-			["<Tab>"] = {
-				-- "select_next",
-				-- "snippet_forward",
-				"fallback",
-			},
-
-			["<S-Tab>"] = {
-				-- "select_prev",
-				-- "snippet_backward",
-				"fallback",
-			},
-		},
-
-		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
-			nerd_font_variant = "mono",
-		},
-
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "dictionary" },
-			providers = {
-				dictionary = {
-					module = "blink-cmp-dictionary",
-					name = "Dict",
-					-- Make sure this is at least 2.
-					-- 3 is recommended
-					min_keyword_length = 3,
-					opts = {
-						-- options for blink-cmp-dictionary
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		"rafamadriz/friendly-snippets", -- Recommended for common snippets
+		"moyiz/blink-emoji.nvim",
+		"archie-judd/blink-cmp-words",
+		{ "echasnovski/mini.icons", version = "*" },
+	},
+	config = function()
+		require("blink.cmp").setup({
+			-- Completion behavior
+			completion = {
+				-- accept = {
+				-- 	-- Select with Enter key
+				-- 	["<CR>"] = { "select_and_accept" },
+				-- },
+				-- menu = { border = "single" },
+				-- { list = { selection = "manual" } },
+				-- documentation = {
+				-- 	auto_show = true,
+				-- 	auto_show_delay_ms = 250,
+				-- 	treesitter_highlight = true,
+				-- 	window = { border = "rounded" },
+				-- },
+				menu = {
+					border = "single",
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+									return kind_icon
+								end,
+								-- (optional) use highlights from mini.icons
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+							kind = {
+								-- (optional) use highlights from mini.icons
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+						},
 					},
 				},
 			},
-		},
-		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = true } },
+			signature = { window = { border = "single" } },
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-		--
-		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
-	opts_extend = { "sources.default" },
+			keymap = {
+				-- preset = "enter",
+				-- preset = "default",
+
+				-- this ones also work fine
+				-- ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+				-- ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+
+				["<CR>"] = { "accept", "fallback" },
+				["<Tab>"] = {
+					function(cmp)
+						return cmp.select_next()
+					end,
+					"snippet_forward",
+					"fallback",
+				},
+				["<S-Tab>"] = {
+					function(cmp)
+						return cmp.select_prev()
+					end,
+					"snippet_backward",
+					"fallback",
+				},
+				["<S-k>"] = { "scroll_documentation_up", "fallback" },
+				["<Esc>"] = { "hide", "fallback" },
+				["<S-j>"] = { "scroll_documentation_down", "fallback" },
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			},
+
+			sources = {
+				-- Default sources for LSP, buffer, snippets, and path.
+				-- blink.cmp often includes these by default, but
+				-- explicitly listing them ensures they are active.
+				default = {
+					"lsp", -- Language Server Protocol completions
+					"buffer", -- Words from current buffer
+					"snippets", -- Snippets from LuaSnip/friendly-snippets
+					"path", -- File system paths
+					"emoji",
+				},
+				-- Providers can be further configured here if needed, e.g.,
+				providers = {
+					emoji = {
+						module = "blink-emoji",
+						name = "Emoji",
+						score_offset = 93, -- the higher the number, the higher the priority
+						min_keyword_length = 2,
+						opts = { insert = true }, -- Insert emoji (default) or complete its name
+					},
+					lsp = {
+						name = "lsp",
+						enabled = true,
+						module = "blink.cmp.sources.lsp",
+						kind = "LSP",
+						min_keyword_length = 2,
+
+						-- When linking markdown notes, I would get snippets and text in the
+						-- suggestions, I want those to show only if there are no LSP
+						-- suggestions
+						--
+						-- Enabled fallbacks as this seems to be working now
+						-- Disabling fallbacks as my snippets wouldn't show up when editing
+						-- lua files
+						-- fallbacks = { "snippets", "buffer" },
+						score_offset = 90, -- the higher the number, the higher the priority
+					},
+					snippets = {
+						name = "snippets",
+						module = "blink.cmp.sources.snippets",
+						max_items = 8,
+					},
+					thesaurus = {
+						name = "blink-cmp-words",
+						module = "blink-cmp-words.thesaurus",
+						-- All available options
+						opts = {
+							-- A score offset applied to returned items.
+							-- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+							score_offset = 0,
+
+							-- Default pointers define the lexical relations listed under each definition,
+							-- see Pointer Symbols below.
+							-- Default is as below ("antonyms", "similar to" and "also see").
+							pointer_symbols = { "!", "&", "^" },
+						},
+					},
+
+					-- Use the dictionary source
+					dictionary = {
+						name = "blink-cmp-words",
+						module = "blink-cmp-words.dictionary",
+						-- All available options
+						opts = {
+							-- The number of characters required to trigger completion.
+							-- Set this higher if completion is slow, 3 is default.
+							dictionary_search_threshold = 3,
+
+							-- See above
+							score_offset = 0,
+
+							-- See above
+							pointer_symbols = { "!", "&", "^" },
+						},
+					},
+				},
+				per_filetype = {
+					text = { "dictionary" },
+					markdown = { "thesaurus" },
+				},
+			},
+
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+
+			snippets = {
+				expand = function(snippet, _)
+					require("luasnip").lsp_expand(snippet)
+				end,
+			},
+		})
+	end,
 }
